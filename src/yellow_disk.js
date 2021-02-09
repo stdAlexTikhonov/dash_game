03699dbc83160bc5b7a945ddf32b09ddcfdd73a1
stdAlexTikhonov <std1992@mail.ru>
2021-02-09 23:44:19 +0300

    TASK-94 Code refactor Yellow Disk

--- src/yellow_disk.js
+++ src/yellow_disk.js
@@ -1,42 +1,16 @@
-import { PLAYER, SCISSORS, EMPTY, YELLOW_DISK, LEFT, RIGHT, ROCK, FOOD, UP, DOWN } from "./constants";
-import { Player } from "./player";
+import { PLAYER, EMPTY, YELLOW_DISK, LEFT, RIGHT, UP, DOWN } from "./constants";
 import disk from "./assets/images/yellow_disk.png"
 
 export class YellowDisk {
     constructor(y,x) {
       this.x = x;
       this.y = y;
-      this.killer = false;
-      this.falling = false;
       this.char = YELLOW_DISK;
-      this.right = false;
-      this.left = false;
       this.img = new Image();
       this.img.src = disk;
       this.still_alive = true;
     }
 
-    check_way_down(world) {
-        if (this.falling && world[this.y + 1][this.x].char === PLAYER) {
-            this.killer = true; Player.off = true;
-        }
-        else if (this.falling && world[this.y + 1][this.x].char === SCISSORS) {
-            this.killer = true;
-        }
-        return  world[this.y+1][this.x].char === EMPTY;
-    }
-
-    move_possible(world) {
-        return ['+', 'O', '*'].includes(world[this.y+1][this.x].char) && !['O', '*'].includes(world[this.y-1][this.x].char)
-    }
-
-    check_way_left(world) {
-        return world[this.y][this.x-1].char === EMPTY && world[this.y+1][this.x-1].char === EMPTY && !['O', '*'].includes(world[this.y-1][this.x-1].char);
-    }
-
-    check_way_right(world) {
-        return world[this.y][this.x+1].char === EMPTY && world[this.y+1][this.x+1].char === EMPTY && !['O', '*'].includes(world[this.y-1][this.x+1].char);
-    }
   
     check_force_move_up(world) {
         return world[this.y-1][this.x].char === EMPTY && world[this.y+1][this.x].char === PLAYER;
@@ -54,12 +28,7 @@ export class YellowDisk {
         return world[this.y][this.x+1].char === EMPTY && world[this.y][this.x-1].char === PLAYER;
     }
   
-    find_death(world) {
-        const killers = [ROCK, FOOD];
-        const pt1 = killers.includes(world[this.y - 1][this.x].char);
-        const pt2 = killers.includes(world[this.y][this.x].char);
-        return pt1;
-    }
+
 
     changeState(world, player) {
 
